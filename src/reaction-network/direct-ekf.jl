afaa4c1a8ec7029507a1d5bf2f6c239c99f9b5f5
Joshua Bon <joshuajbon@gmail.com>
2023-08-29 16:23:41 +1000

    More work on reaction networks

---
+++ src/reaction-network/direct-ekf.jl
@@ -0,0 +1,126 @@
+
+mutable struct GaussianFilter
+    μ::Vector{<:Real}
+    Σ::AbstractMatrix{<:Real}
+end
+
+struct KalmanEM
+    drift::Function
+    noise::Function
+    jac::Function
+    # sdrift::Vector{<:Real}
+    # snoise::AbstractMatrix{<:Real}
+    # sjac::AbstractMatrix{<:Real}
+end
+
+function predict!(x::GaussianFilter, kem::KalmanEM, p::Vector{<:Real}, dt::Float64; t::Real = 0.0, nugget::Real = 0.0)
+    
+    μp = x.μ + kem.drift(x.μ, p) .* dt
+    F = kem.jac(x.μ, p)
+
+    L = kem.noise(x.μ, p) 
+    Q = L * L'
+    Σp = x.Σ + ((F * x.Σ + x.Σ * F') + Q ).* dt + (nugget * I)
+    
+    x.μ = μp
+    x.Σ = Symmetric(Σp)
+
+end
+
+function update!(x::GaussianFilter, y::Vector{<:Real}, H::AbstractMatrix{<:Real}, R::AbstractMatrix{<:Real}; nugget::Real = 0.0)
+
+    yres = y - H * x.μ
+    S = H * x.Σ * H' + R
+    ll = logpdf(MvNormal(zero(yres), S), yres)
+
+    K = x.Σ * H' / S
+    μu = x.μ + K * yres
+    Σu = (I - K*H) * x.Σ + (nugget * I)
+
+    x.μ = μu
+    x.Σ = Symmetric(Σu)
+
+    return ll
+
+end
+
+struct Observation
+    y::Vector{<:Real}
+    t::Float64
+end
+
+
+function kalman!(x::GaussianFilter, t0::Float64, kem::KalmanEM, H::AbstractMatrix{<:Real}, R::AbstractMatrix{<:Real}, p::Vector{<:Real}, observations::Vector{Observation}, dt0::Float64; nugget::Tuple{Float64, Float64} = (0.0,0.0))
+    # takes step sizes dt or to the nearest observation time if closer
+    # t = t0, t0 + dt, ..., t1, t1 + dt, t1 + 2dt, ... , tn
+    ll = 0.0
+
+    # initial time
+    t = t0
+    for obs in observations
+        # predict at dt increments
+        while t < obs.t
+            # next increment
+            dt = min(dt0, obs.t - t)
+
+            # update to time t + dt
+            predict!(x, kem, p, dt, t = t, nugget = nugget[1])
+            t += dt
+        end
+
+        # update
+        ll = ll + update!(x, obs.y, H, R, nugget = nugget[2])
+        #println("$t")
+    end
+
+    return ll
+
+end
+
+struct KalmanApproxSDE
+    model::KalmanEM
+    observations::Vector{Observation}
+    t0::Float64 # initial time
+    dt0::Float64 # (max) time increments
+    H::AbstractMatrix{<:Real} # y = Hx + ε
+    x0::GaussianFilter # initial distirbution
+    xscratch::GaussianFilter # scratch space for filter
+end
+
+KalmanApproxSDE(model::KalmanEM, observations::Vector{Observation}, t0::Float64, dt0::Float64, H::AbstractMatrix{<:Real}, x0::GaussianFilter) = KalmanApproxSDE(model, observations, t0, dt0, H, x0, GaussianFilter(similar(x0.μ), similar(x0.Σ)))
+
+
+struct KalmanSolution
+    x::GaussianFilter # final distribution
+    logZ::Real
+    info::Int64
+end
+
+# unknown prior mean covariance
+function (k::KalmanApproxSDE)(p::Vector{<:Real}, R::AbstractMatrix{<:Real}, μ0::AbstractVector{<:Real}, Σ0::AbstractMatrix{<:Real}; nugget::Tuple{Float64, Float64} = (0.0,0.0))
+    # initialise scratch
+    k.xscratch.μ = μ0
+    k.xscratch.Σ = Σ0
+    
+    # run Kalman Filter
+    info = 0
+    loglike = 0.0
+    
+    try
+        loglike = kalman!(k.xscratch, k.t0, k.model, k.H, R, p, k.observations, k.dt0; nugget = nugget)
+    catch err
+        if err isa PosDefException
+            info = 1
+        elseif err isa SingularException
+            info = 2
+        else 
+            info = 99
+            throw(err)
+        end
+    end
+
+    return KalmanSolution(deepcopy(k.xscratch), loglike, info)
+end
+
+# fixed prior mean covariance
+(k::KalmanApproxSDE)(p::Vector{<:Real}, R::AbstractMatrix{<:Real}; nugget::Tuple{Float64, Float64} = (0.0,0.0)) = k(p, R, k.x0.μ, k.x0.Σ, nugget = nugget)
--- src/reaction-network/reaction-test.jl
+++ src/reaction-network/reaction-test.jl
@@ -5,11 +5,12 @@ using Catalyst
 using LinearAlgebra
 using DifferentialEquations
 using Turing
+using SparseArrays
 
 getparams(m::DynamicPPL.Model) = DynamicPPL.syms(DynamicPPL.VarInfo(m))
 getstatesymbol(x::SymbolicUtils.BasicSymbolic) = x.metadata[Symbolics.VariableSource][2]
 
-include("ekf.jl")
+include("direct-ekf.jl")
 
 mapk_2step = @reaction_network begin
     k1, X + E --> XE
@@ -71,7 +72,7 @@ u0 = Dict(
 )
 
 
-ord_u0 = [u0[getstatesymbol(x)] for x in states(mapk_2step)]
+ord_u0 = Float64.([u0[getstatesymbol(x)] for x in states(mapk_2step)])
 
 times = 0:4:200
 otimes = times[2:end]
@@ -89,38 +90,126 @@ jsol = solve(jprob, SSAStepper())
 obsv = [Observation(jsol(t)[observedid], t) for t in otimes]
 gstates = GaussianFilter(jsol(0.0)[observedid], R)
 
-mapk_sde_sys = convert(SDESystem, mapk_2step)
+mapk_sde_sys = convert(SDESystem, mapk_2step; combinatoric_ratelaws=true)
 mapk_sde = SDEFunction(mapk_sde_sys, jac = true)
 
+mapk_sde_sys.eqs
+mapk_sde_sys.noiseeqs
+
+modelsymdrift = getfield.(mapk_sde_sys.eqs, :rhs)
+modelsymjac = Symbolics.jacobian(modelsymdrift, states(mapk_2step))
+modelsymnoise = mapk_sde_sys.noiseeqs
+ss = simplify(modelsymnoise * modelsymnoise')
+
+ss[1,1]
+
+function mapk2drift(u::AbstractVector, p::AbstractVector)
+    [p[2]*u[3] + p[6]*u[6] - p[1]*u[2]*u[1],  # X
+    p[2]*u[3] + p[3]*u[3] - p[1]*u[2]*u[1],  # E
+    p[1]*u[2]*u[1] - p[2]*u[3] - p[3]*u[3],  # XE
+    p[3]*u[3] + p[5]*u[6] + p[8]*u[8] + p[9]*u[8] - p[4]*u[5]*u[4] - p[7]*u[4]*u[7], # Xᵃ
+    p[5]*u[6] + p[6]*u[6] - p[4]*u[5]*u[4],  # P₁
+    p[4]*u[5]*u[4] - p[5]*u[6] - p[6]*u[6],  # XᵃP₁
+    p[12]*u[11] + p[8]*u[8] - p[7]*u[4]*u[7], # Y
+    p[7]*u[4]*u[7] - p[8]*u[8] - p[9]*u[8],  # XᵃY
+    p[9]*u[8] + p[11]*u[11] - p[10]*u[10]*u[9],    # Yᵃ
+    p[11]*u[11] + p[12]*u[11] - p[10]*u[10]*u[9], # P₂
+    p[10]*u[10]*u[9] - p[11]*u[11] - p[12]*u[11]] # YᵃP₂
+end
+
+function mapk2noise(u::AbstractVector, p::AbstractVector)
+    [-sqrt(abs(p[1]*u[2]*u[1])) sqrt(abs(p[2]*u[3])) 0. 0. 0. sqrt(abs(p[6]*u[6])) 0. 0. 0. 0. 0. 0.; 
+    -sqrt(abs(p[1]*u[2]*u[1])) sqrt(abs(p[2]*u[3])) sqrt(abs(p[3]*u[3])) 0. 0. 0. 0. 0. 0. 0. 0. 0.;
+    sqrt(abs(p[1]*u[2]*u[1])) -sqrt(abs(p[2]*u[3])) -sqrt(abs(p[3]*u[3])) 0. 0. 0. 0. 0. 0. 0. 0. 0.; 
+    0. 0. sqrt(abs(p[3]*u[3])) -sqrt(abs(p[4]*u[5]*u[4])) sqrt(abs(p[5]*u[6])) 0. -sqrt(abs(p[7]*u[4]*u[7])) sqrt(abs(p[8]*u[8])) sqrt(abs(p[9]*u[8])) 0. 0. 0.;
+    0. 0. 0. -sqrt(abs(p[4]*u[5]*u[4])) sqrt(abs(p[5]*u[6])) sqrt(abs(p[6]*u[6])) 0. 0. 0. 0. 0. 0.; 
+    0. 0. 0. sqrt(abs(p[4]*u[5]*u[4])) -sqrt(abs(p[5]*u[6])) -sqrt(abs(p[6]*u[6])) 0. 0. 0. 0. 0. 0.;
+    0. 0. 0. 0. 0. 0. -sqrt(abs(p[7]*u[4]*u[7])) sqrt(abs(p[8]*u[8])) 0. 0. 0. sqrt(abs(p[12]*u[11])); 
+    0. 0. 0. 0. 0. 0. sqrt(abs(p[7]*u[4]*u[7])) -sqrt(abs(p[8]*u[8])) -sqrt(abs(p[9]*u[8])) 0. 0. 0.;
+    0. 0. 0. 0. 0. 0. 0. 0. sqrt(abs(p[9]*u[8])) -sqrt(abs(p[10]*u[10]*u[9])) sqrt(abs(p[11]*u[11])) 0.; 
+    0. 0. 0. 0. 0. 0. 0. 0. 0. -sqrt(abs(p[10]*u[10]*u[9])) sqrt(abs(p[11]*u[11])) sqrt(abs(p[12]*u[11]));
+    0. 0. 0. 0. 0. 0. 0. 0. 0. sqrt(abs(p[10]*u[10]*u[9])) -sqrt(abs(p[11]*u[11])) -sqrt(abs(p[12]*u[11]))]
+end
+
+function mapk2noise_sparse(u::AbstractVector, p::AbstractVector)
+    rw = [1, 2, 3, 1, 2, 3, 2, 3, 4, 4, 5, 6, 4, 5, 6, 1, 5, 6, 4, 7, 8, 4, 7, 8, 4, 8, 9, 9, 10, 11, 9, 10, 11, 7, 10, 11]
+    cl = [1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 8, 9, 9, 9, 10, 10, 10, 11, 11, 11, 12, 12, 12]
+
+    z = [
+        sqrt(abs(p[1]*u[2]*u[1])),
+        sqrt(abs(p[2]*u[3])), 
+        sqrt(abs(p[3]*u[3])), 
+        sqrt(abs(p[4]*u[5]*u[4])), 
+        sqrt(abs(p[5]*u[6])),
+        sqrt(abs(p[6]*u[6])),
+        sqrt(abs(p[7]*u[4]*u[7])),
+        sqrt(abs(p[8]*u[8])),
+        sqrt(abs(p[9]*u[8])),
+        sqrt(abs(p[10]*u[10]*u[9])),
+        sqrt(abs(p[11]*u[11])),
+        sqrt(abs(p[12]*u[11]))
+    ]
+
+    el = [-z[1],-z[1],z[1],z[2],z[2],-z[2],z[3],-z[3],z[3],-z[4],-z[4],z[4],z[5],z[5],-z[5],z[6],z[6],-z[6],-z[7],-z[7],z[7],z[8],z[8],-z[8],z[9],-z[9],z[9],-z[10],-z[10],z[10],z[11],z[11],-z[11],z[12],z[12],-z[12]]
+
+    return sparse(rw,cl,el)
+
+end
+
+# check
+all(mapk2noise_sparse(ord_u0 .+0.01, p) .== mapk2noise(ord_u0 .+0.01, p))
 
 
-# u = rand(length(states(mapk_2step)))
-# t = 0
-# mapk_sde.f(u, p, t)
-# L = mapk_sde.g(u, p, t)
+function mapk2jacobian(u::AbstractVector, p::AbstractVector)
+    [-p[1]*u[2] -p[1]*u[1] p[2] 0. 0. p[6] 0. 0. 0. 0. 0.; 
+    -p[1]*u[2] -p[1]*u[1] p[2] + p[3] 0. 0. 0. 0. 0. 0. 0. 0.; 
+    p[1]*u[2] p[1]*u[1] -p[2] - p[3] 0. 0. 0. 0. 0. 0. 0. 0.; 
+    0. 0. p[3] -p[4]*u[5] - p[7]*u[7] -p[4]*u[4] p[5] -p[7]*u[4] p[8] + p[9] 0. 0. 0.; 
+    0. 0. 0. -p[4]*u[5] -p[4]*u[4] p[5] + p[6] 0. 0. 0. 0. 0.; 
+    0. 0. 0. p[4]*u[5] p[4]*u[4] -p[5] - p[6] 0. 0. 0. 0. 0.; 
+    0. 0. 0. -p[7]*u[7] 0. 0. -p[7]*u[4] p[8] 0. 0. p[12]; 
+    0. 0. 0. p[7]*u[7] 0. 0. p[7]*u[4] -p[8] - p[9] 0. 0. 0.; 
+    0. 0. 0. 0. 0. 0. 0. p[9] -p[10]*u[10] -p[10]*u[9] p[11]; 
+    0. 0. 0. 0. 0. 0. 0. 0. -p[10]*u[10] -p[10]*u[9] p[11] + p[12]; 
+    0. 0. 0. 0. 0. 0. 0. 0. p[10]*u[10] p[10]*u[9] -p[11] - p[12]]
+end
 
-# mapk_sde.jac(u, p, t)
+function mapk2jacobian_sparse(u::AbstractVector, p::AbstractVector)
+    rw = [1, 2, 3, 1, 2, 3, 1, 2, 3, 4, 4, 5, 6, 7, 8, 4, 5, 6, 1, 4, 5, 6, 4, 7, 8, 4, 7, 8, 9, 9,  10, 11, 9,  10, 11, 7,  9,  10, 11]
+    cl = [1, 1, 1, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 10, 10, 10, 11, 11, 11, 11]
 
-# generate_jacobian(mapk_sde_sys)
+    z = [p[1]*u[2], p[1]*u[1], p[4]*u[5], p[7]*u[7], p[4]*u[4], p[7]*u[4], p[10]*u[10], p[10]*u[9]]
 
-# inv(L * L' + 0.01*I)
+    el = [-z[1], -z[1], z[1], -z[2], -z[2], z[2], p[2], p[2] + p[3], -p[2] - p[3], p[3], -z[3] - z[4], -z[3], z[3], -z[4], z[4], 
+    -z[5], -z[5], z[5], p[6], p[5], p[5] + p[6], -p[5] - p[6], -z[6], -z[6], z[6], p[8] + p[9], p[8], -p[8] - p[9], p[9], -z[7], 
+    -z[7], z[7], -z[8], -z[8], z[8], p[12], p[11], p[11] + p[12], -p[11] - p[12]]
+    return sparse(rw,cl,el)
+end
 
+all(mapk2jacobian_sparse(ord_u0 .+0.01, p) .== mapk2jacobian(ord_u0 .+0.01, p))
+
+
+mapk2kem = KalmanEM(mapk2drift, mapk2noise_sparse, mapk2jacobian_sparse)
+
+#mapk2kem.drift(ord_u0, p)
+#mapk2kem.noise(ord_u0, p)
+#mapk2kem.jac(ord_u0, p)
 
 
 g = GaussianFilter(zeros(nstates), Diagonal(ones(nstates)))
 
-predict!(g, mapk_sde, p, 0.01, nugget = 0.01)
+predict!(g, mapk2kem, p, 0.01, nugget = 0.01)
 
 ll = update!(g, rand(nstates), Diagonal(ones(nstates)), Diagonal(ones(nstates)))
 
 
 g = GaussianFilter(ord_u0, Diagonal(0.5 * sqrt.(max.(ord_u0, 1.0))))
 
-kalman!(g, 0.0, mapk_sde, H, R, p.+ 0.000001, obsv, 0.1, nugget = (0.0, 0.0))
+kalman!(g, 0.0, mapk2kem, H, R, p.+ 0.000001, obsv, 0.1, nugget = (0.0, 0.0))
 
 
-x = GaussianFilter(ord_u0, Diagonal(0.5 * sqrt.(max.(ord_u0, 1.0))))
-kfsde = KalmanApproxSDE(mapk_sde, obsv, 0.0, 0.1, H, x)
+x = GaussianFilter(ord_u0 .+ 0.001, Diagonal(0.5 * sqrt.(max.(ord_u0, 1.0))))
+kfsde = KalmanApproxSDE(mapk2kem, obsv, 0.0, 1.0, H, x)
 
 kfsde(p, R).logZ
 kfsde(p, R).info
@@ -138,22 +227,23 @@ reorder_id = [get(par_id, v, 0) for v in ord_parameters]
 
 @model kalman_model(kfsde::KalmanApproxSDE) = begin
      
-    k = tzeros(12)
-    k[1] = param_fixed[:k1]
-    k[2] = par[:k2] #~ Uniform(0.0, par[:k1]) 
-    k[3] ~ Uniform(0.0, 1.0)    
-    k[4] = param_fixed[:k4]  
-    k[5] = par[:k5] #~ Uniform(0.0, par[:k4])  
-    k[6] ~ Uniform(0.0, 1.0)
-    k[7] = param_fixed[:k7]  
-    k[8] = par[:k8] #~ Uniform(0.0, par[:k7])   
-    k[9] ~ Uniform(0.0, 1.0)
-    k[10] = param_fixed[:k10]     
-    k[11] = par[:k11] #~ Uniform(0.0, par[:k10]) 
-    k[12] ~ Uniform(0.0, 1.0)          
+    k1 = param_fixed[:k1]
+    k2 = par[:k2] #~ Uniform(0.0, par[:k1]) 
+    k3 ~ Uniform(0.0, 1.0)    
+    k4 = param_fixed[:k4]  
+    k5 = par[:k5] #~ Uniform(0.0, par[:k4])  
+    k6 ~ Uniform(0.0, 1.0)
+    k7 = param_fixed[:k7]  
+    k8 = par[:k8] #~ Uniform(0.0, par[:k7])   
+    k9 ~ Uniform(0.0, 1.0)
+    k10 = param_fixed[:k10]     
+    k11 = par[:k11] #~ Uniform(0.0, par[:k10]) 
+    k12 ~ Uniform(0.0, 1.0)          
 
-    σ = 1.0
-    sol = kfsde(k[reorder_id], Diagonal(I * σ^2, nobs))
+    k = [k1, k2, k3, k4, k5, k6, k7, k8, k9, k10, k11, k12]
+
+    σ = 0.01
+    sol = kfsde(k, Diagonal(I * σ^2, nobs))
     
     if sol.info == 0
         Turing.@addlogprob! sol.logZ
@@ -171,3 +261,121 @@ ch_approx_mod = sample(approx_mod, NUTS(), 1000, init_params = [par[:k3], par[:k
 DynamicPPL.VarInfo(approx_mod)
 
 # try laplace approximation instead
+
+reorder_id
+
+
+k1 = p
+k1[3] = 0.18 - 0.005
+sol = kfsde(k1[reorder_id], Diagonal(I * 1.0, nobs))
+sol.logZ
+
+@profile kfsde(p, Diagonal(I * 1.0, nobs))
+
+using ForwardDiff
+
+f(x) = sum(mapk2drift(ord_u0, x) .* ones(11))
+ForwardDiff.gradient(x -> sum(mapk2drift(ord_u0, x) .* ones(11)), p)
+
+
+ForwardDiff.gradient(x -> sum(mapk2noise(ord_u0 .+ 0.001, x) * ones(12)), p)
+ForwardDiff.gradient(x -> sum(mapk2jacobian(ord_u0, x) * ones(11)), p)
+ForwardDiff.gradient(x -> kfsde(x, R).logZ, p)
+
+pp = remake(sdeprob, tspan = (0.0,1.0), u0 = uu)
+
+sol = solve(pp, EM(), dt = 0.1)
+sol(0.1)
+
+SciMLSensitivity.SDEAdjointProblem(sol, BacksolveAdjoint(), )
+
+
+generate_jacobian(sdeprob, u0, k1)
+
+
+zz = zeros(Real, 11)
+function f(x) 
+    vv = merge(Dict(parameters(mapk_2step) .=> x), Dict(states(mapk_2step) .=> uu))
+    zz = Symbolics.value.(substitute(modelsymjac, vv))
+    sum(zz)
+end
+
+ForwardDiff.gradient(f, k1)
+
+(zz, uu, k1)
+
+
+solve(prob, EM(), dt = dt)
+
+# check if differentiable 
+
+
+
+zz = mapk2noise(ord_u0 .+ 0.01, p)
+
+indzz = CartesianIndices(zz)[.! iszero.(zz)]
+
+repr(map(x -> x.I[1], indzz))
+repr(map(x -> x.I[2], indzz))
+
+
+["-sqrt(abs(p[1]*u[2]*u[1]))" "sqrt(abs(p[2]*u[3]))" "0" "0" "0" sqrt(abs(p[6]*u[6])) "0" "0" "0" "0" "0" "0"; 
+"-sqrt(abs(p[1]*u[2]*u[1]))" "sqrt(abs(p[2]*u[3]))" "sqrt(abs(p[3]*u[3]))" "0" "0" "0" "0" "0" "0" "0" "0" "0";
+"sqrt(abs(p[1]*u[2]*u[1]))" "-sqrt(abs(p[2]*u[3]))" "-sqrt(abs(p[3]*u[3]))" "0" "0" "0" "0" "0" "0" "0" "0" "0"; 
+"0" "0" "sqrt(abs(p[3]*u[3]))" "-sqrt(abs(p[4]*u[5]*u[4]))" sqrt(abs(p[5]*u[6])) "0" -sqrt(abs(p[7]*u[4]*u[7])) sqrt(abs(p[8]*u[8])) sqrt(abs(p[9]*u[8])) "0" "0" "0";
+"0" "0" "0" -sqrt(abs(p[4]*u[5]*u[4])) sqrt(abs(p[5]*u[6])) sqrt(abs(p[6]*u[6])) "0" "0" "0" "0" "0" "0"; 
+"0" "0" "0" sqrt(abs(p[4]*u[5]*u[4])) -sqrt(abs(p[5]*u[6])) -sqrt(abs(p[6]*u[6])) "0" "0" "0" "0" "0" "0";
+"0" "0" "0" "0" "0" "0" -sqrt(abs(p[7]*u[4]*u[7])) sqrt(abs(p[8]*u[8])) "0" "0" "0" sqrt(abs(p[12]*u[11])); 
+"0" "0" "0" "0" "0" "0" sqrt(abs(p[7]*u[4]*u[7])) -sqrt(abs(p[8]*u[8])) -sqrt(abs(p[9]*u[8])) "0" "0" "0";
+"0" "0" "0" "0" "0" "0" "0" "0" sqrt(abs(p[9]*u[8])) -sqrt(abs(p[10]*u[10]*u[9])) sqrt(abs(p[11]*u[11])) "0"; 
+"0" "0" "0" "0" "0" "0" "0" "0" "0" -sqrt(abs(p[1"0"]*u[10]*u[9])) sqrt(abs(p[11]*u[11])) sqrt(abs(p[12]*u[11]));
+"0" "0" "0" "0" "0" "0" "0" "0" "0" sqrt(abs(p[10]*u[10]*u[9])) -sqrt(abs(p[11]*u[11])) -sqrt(abs(p[12]*u[11]))]
+
+[-sqrt(abs(p[1]*u[2]*u[1])), -sqrt(abs(p[1]*u[2]*u[1])), sqrt(abs(p[1]*u[2]*u[1])), 
+sqrt(abs(p[2]*u[3])), sqrt(abs(p[2]*u[3])), -sqrt(abs(p[2]*u[3])),
+sqrt(abs(p[3]*u[3])), -sqrt(abs(p[3]*u[3])), sqrt(abs(p[3]*u[3])),
+-sqrt(abs(p[4]*u[5]*u[4]))
+]
+
+
+
+@profile mapk2noise(ord_u0 .+ 0.01 , p)
+
+
+
+
+
+
+M = [
+-a b  0. 0. 0. f  0. 0. 0. 0. 0. 0.; 
+-a b  c  0. 0. 0. 0. 0. 0. 0. 0. 0.;
+a -b -c  0. 0. 0. 0. 0. 0. 0. 0. 0.; 
+0. 0. c -d  e  0 -g  h  i  0. 0. 0.;
+0. 0. 0 -d  e  f  0. 0. 0. 0. 0. 0.; 
+0. 0. 0. d -e -f  0. 0. 0. 0. 0. 0.;
+0. 0. 0. 0. 0. 0 -g  h  0. 0. 0. l ; 
+0. 0. 0. 0. 0. 0. g -h -i  0. 0. 0.;
+0. 0. 0. 0. 0. 0. 0. 0. i -j  k  0.; 
+0. 0. 0. 0. 0. 0. 0. 0. 0 -j  k  l ;
+0. 0. 0. 0. 0. 0. 0. 0. 0. j -k -l
+]
+
+rw = [1, 2, 3, 1, 2, 3, 2, 3, 4, 4, 5, 6, 4, 5, 6, 1, 5, 6, 4, 7, 8, 4, 7, 8, 4, 8, 9, 9, 10, 11, 9, 10, 11, 7, 10, 11]
+cl = [1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 8, 9, 9, 9, 10, 10, 10, 11, 11, 11, 12, 12, 12]
+
+z = [
+    sqrt(abs(p[1]*u[2]*u[1])),
+    sqrt(abs(p[2]*u[3])), 
+    sqrt(abs(p[3]*u[3])), 
+    sqrt(abs(p[4]*u[5]*u[4])), 
+    sqrt(abs(p[5]*u[6])),
+    sqrt(abs(p[6]*u[6])),
+    sqrt(abs(p[7]*u[4]*u[7])),
+    sqrt(abs(p[8]*u[8])),
+    sqrt(abs(p[9]*u[8])),
+    sqrt(abs(p[10]*u[10]*u[9])),
+    sqrt(abs(p[11]*u[11])),
+    sqrt(abs(p[12]*u[11]))
+]
+
+el = [-z[1],-z[1],z[1],z[2],z[2],-z[2],z[3],-z[3],z[3],-z[4],-z[4],z[4],z[5],z[5],-z[5],z[6],z[6],-z[6],-z[7],-z[7],z[7],z[8],z[8],-z[8],z[9],-z[9],z[9],-z[10],-z[10],z[10],z[11],z[11],-z[11],z[12],z[12],-z[12]]
